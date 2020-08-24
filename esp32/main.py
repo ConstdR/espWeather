1fe1ff2aafeb76680bed0ed7461802cb8dd2b4ab
ConstdR <deribin@gmail.com>
2020-08-24 16:44:51 +0200

    Use one dht meassure

--- esp32/main.py
+++ esp32/main.py
@@ -10,15 +10,13 @@ myID = int.from_bytes(machine.unique_id(), 'big') # 'little' is more correct :)
 led = machine.Pin(21, machine.Pin.OUT)
 dht = dht.DHT11(machine.Pin(32))
 dhtpower = machine.Pin(12, machine.Pin.OUT)
-MEASURE_COUNT = 3
+MEASURE_COUNT = 1
 MEASURE_TIMEOUT = 1
 DEEP_SLEEP = 900000 # 900000 == 15 min
 
 def main():
     while True:
-        dhtpower.on()
         (t, h) = measure()
-        dhtpower.off()
         url = URL % (myID, t, h)
         print("Get: %s" % url)
         try:
@@ -34,12 +32,13 @@ def main():
 def measure(res = [0, 0]):
     # DHT have more than 2 sec sampling period 
     # https://www.makerguides.com/wp-content/uploads/2019/02/DHT11-Datasheet.pdf
-    # so we do 3 measures in 1 sec interval and take average (but we still give a shit)
+    # so we can do MEASURE_COUNT measures in MEASURE_TIMEOUT sec interval 
+    # and take average (but we still give a shit)
     dhtpower.on()
     try:
-        dht.measure()
         for i in range(MEASURE_COUNT):
             time.sleep(MEASURE_TIMEOUT)
+            dht.measure()
             res[0] = res[0] + dht.temperature()
             res[1] = res[1] + dht.humidity()
         res[0] = res[0] / MEASURE_COUNT
