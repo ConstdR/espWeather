2a5c3dcfa93da1261bc7ec12b595e9dbadc53b71
ConstdR <deribin@gmail.com>
2020-09-18 16:52:56 +0200

    Switch data storage to sqlite3 and handle data display by tWeb

--- README.md
+++ README.md
@@ -1,32 +1,36 @@
 Here we go with weather sensor based on ESP32 SoC.
 
-Board powered by Li-Ion 18650 battery on 3.3V pin.
+Board powered by Li-Ion 18650 battery.
 
 BME280 sensor connection: SDA - Pin 23, SCL - Pin 22
 
-AP-configuration mode starts automatically, if no config file found or by reset board with lowered Pin 15. 
+AP-configuration mode starts automatically, if no config file found or by reset board with lowered Pin 35.
 At this mode you can connect to WiFi AP with name like ESP_XXXXX w/o password and set your local WiFi credentials and IP:PORT of the server where to send data.
+Time automatically adjusted with NTP server.
 
-Pin 34 reads adc from 470kOm/470kOm devider between 3.3V and GND pin.
+Pin 34 reads adc from 470kOm/470kOm devider between + Li-Ion and GND pin.
 
-tWeb.py -- simple web server to store data recieved from ESP32 modules. Format: CSV. GET requests expected with mandatory 'id' param. File name determined by id field. Columns are:
-- UTC time
-- IP
+tWeb.py -- simple web server to store data recieved from ESP32 modules and show current values and history graphs.
+Storage format: sqlite3.
+Data stored by GET requests from ESP32 with mandatory 'id' param and optinal 't' (temperature), 'h' (humidity), 'p' (pressure),
+'v' (voltage), 'm' (message). File name determined by id field 'id'.sqlite3. 'data' table contain columns:
+- timedate - UTC time
+- ip
 - temperature (t)
 - humidity (h)
 - pressure (p) in hPa
-- Battery (v) relative value, must be recalculated
-
+- voltage (v) relative value, must be recalculated to real V. (2420 is near full charge, 1515 -- depleated)
+Measurement performs once per 900 seconds (15 min) and most of time ESP spend in deep-sleep mode.
 
 *Hints.*
 Using Adafruit MicroPython Tool (ampy)
 
 Put files to ESP32
 
-# ~/.local/bin/ampy -p /dev/ttyUSB0 put boot.py 
-# ~/.local/bin/ampy -p /dev/ttyUSB0 put ap.py 
-# ~/.local/bin/ampy -p /dev/ttyUSB0 put main.py 
-# ~/.local/bin/ampy -p /dev/ttyUSB0 put BME280.py 
+# ~/.local/bin/ampy -p /dev/ttyUSB0 put boot.py
+# ~/.local/bin/ampy -p /dev/ttyUSB0 put ap.py
+# ~/.local/bin/ampy -p /dev/ttyUSB0 put main.py
+# ~/.local/bin/ampy -p /dev/ttyUSB0 put BME280.py
 
 Connect to board on USB:
 
--- tWeb/tweb.py
+++ tWeb/tweb.py
@@ -10,6 +10,7 @@ import logging
 from http.server import *
 from urllib.parse import urlparse, parse_qs
 from datetime import datetime
+import os, sqlite3
 
 lg = logging.getLogger(__file__)
 args = None
@@ -22,102 +23,133 @@ def main():
     parser.add_argument('--log_file', dest="log_file", help="logging file. default - stderr")
     parser.add_argument('--hostname', dest="hostname", default='', help="web hostname (ip address). default ''")
     parser.add_argument('--port', dest="port", default=8088, help="web port. default 8088")
-    parser.add_argument('--dir', dest="dir", default='./', help="directory to store params")
+    parser.add_argument('--datadir', dest='datadir', default='./', help="diredtory to store data. default: './'")
+    parser.add_argument('--timezone', dest='timezone', default='+2', help="timezone in hours i.e: +2 or -8 and etc. default: '+2'")
+    parser.add_argument('--dir', dest="dir", default='./', help="directory to store params. default: './'")
 
     args = parser.parse_args()
 
     loggerConfig(level=args.verbose_count, log_file=args.log_file)
-
-# 'application' code
-#    lg.debug('debug message')
-#    lg.info('info message')
-#    lg.warning('warn message')
-#    lg.error('error message')
-#    lg.critical('critical message')
-#    print("args:" , args)
-
-    httpd = HTTPServer( ( args.hostname, args.port ) , tHandler )
+    httpd = HTTPServer((args.hostname, args.port), tHandler)
     httpd.serve_forever()
 
 class tHandler(BaseHTTPRequestHandler):
-    def do_HEAD(self, code=200):
-        self.send_response(code)
-        self.send_header("Content-type", "text/text")
-        self.end_headers()
-    def do_GET(self):
+    code = 200
+    def show_favicon_ico(self, data):
+        self.do_HEAD('text/text')
+        self.wfile.write(b'')
+
+    def show_graph(self, data):
+        lg.debug("Graph for %s" % data)
+        txt = ''
+        content_type = 'text/text'
         try:
-            params = parse_qs(urlparse(self.path).query)
-            client_ip = self.client_address[0]
-            lg.debug((client_ip, params))
-            code = 200
-    
-            if 'id' in params.keys():
-                remoteid = params['id'][0]
-                temp = params['t'][0] if 't' in params.keys() else '0'
-                humidity = params['h'][0] if 'h' in params.keys() else '0'
-                pressure = params['p'][0] if 'p' in params.keys() else '0'
-                voltage = params['v'][0] if 'v' in params.keys() else '0'
-                timestamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-                row = "%s,%s,%s,%s,%s,%s" % (timestamp, client_ip, temp, humidity, pressure, voltage)
-                lastrow = None
-                try:
-                    fr = open("%s/%s.csv" % (args.dir, remoteid), 'r')
-                    # Chrome love to re-send requests
-                    # get rid of them
-                    lastrow = tail(fr) 
-                    if lastrow[0] is not None :
-                        lastrow = lastrow[0][0].replace(r"\n", " ") 
-                    fr.close()
-                except Exception as e:
-                    lg.error("%s", e)
-    
-                if lastrow is None or lastrow != row:
-                    try:
-                        fh = open("%s/%s.csv" % (args.dir, remoteid), 'a')
-                        fh.write("%s\n" % row)
-                        lg.debug(row)
-                        fh.close()
-                    except Exception as e:
-                        lg.critical("%s", e)
+            if data[0] in ('dygraph.js', 'dygraph.css'):
+                txt = open('%s/templates/%s' % (args.datadir, data[0]), 'r').read()
+                if data[0].endswith('.css'):
+                    content_type = 'text/css'
                 else:
-                    lg.debug("Ignoring duplicate row")
+                    content_type = 'text/javascript'
+            elif len(data) > 1 and data[1].endswith('.csv'):
+                dbh = get_dbh(data[0])
+                content_type = 'text/csv'
+                res = dbh.execute("select *, datetime(timedate, '%s hours') as tztime from data order by timedate"
+                                   % args.timezone)
+                rows = res.fetchall()
+                for row in rows:
+                    txt = txt + "%(tztime)s,%(ip)s,%(temperature)s,%(humidity)s,%(pressure)s,%(voltage)s\n" % row 
+                dbh.close()
             else:
-                lg.debug("No id key found")
-                code = 400
+                dbh = get_dbh(data[0])
+                res = dbh.execute("select *, datetime(timedate, '%s hours') as tztime from data order by timedate desc limit 1"
+                                  % args.timezone )
+                row = res.fetchone()
+                row['id'] = data[0]
+                lg.debug(row)
+                tmpl = open('%s/templates/graph.tmpl' % args.datadir, 'r').read()
+                txt = tmpl % row
+                content_type = 'text/html'
+                dbh.close()
         except Exception as e:
-                lg.error("GET error: %s" % e )
-                code = 500
-    
-        self.do_HEAD(code)
-        self.wfile.write(b'OK.')
+            lg.error(str(e))
+            self.code = 400
+            txt = 'Error'
+        btxt = bytearray(txt, 'UTF-8')
+        self.do_HEAD(content_type, len(btxt))
+        self.wfile.write(btxt)
 
+    def do_HEAD(self, content_type="text/text", content_length=None):
+        self.send_response(self.code)
+        self.send_header("Content-type", content_type)
+        if content_length :
+            self.send_header("Content_length", content_length)
+        self.end_headers()
 
-#############################
+    def do_GET(self):
+        parsedurl = urlparse(self.path)
+        parsedpath = parsedurl.path.split('/')
+        parsedpath.pop(0)
+        lg.debug("parsedurl:")
+        lg.debug(parsedpath)
 
-def tail(f, n=1, offset=None):
-    """Reads a n lines from f with an offset of offset lines.  The return
-    value is a tuple in the form ``(lines, has_more)`` where `has_more` is
-    an indicator that is `True` if there are more lines in the file.
-    https://stackoverflow.com/questions/136168/get-last-n-lines-of-a-file-similar-to-tail/692616#692616
-    """
-    avg_line_length = 30
-    to_read = n + (offset or 0)
-
-    while 1:
         try:
-            f.seek(-(avg_line_length * to_read), 2)
-        except IOError:
-            # woops.  apparently file is smaller than what we want
-            # to step back, go to the beginning instead
-            f.seek(0)
-        pos = f.tell()
-        lines = f.read().splitlines()
-        if len(lines) >= to_read or pos == 0:
-            return lines[-to_read:offset and -offset or None], \
-                   len(lines) > to_read or pos > 0
-        avg_line_length *= 1.3
+            method = 'show_' + parsedpath[0].replace('.', '_')
+            parsedpath.pop(0)
+            result = getattr(self, method)(parsedpath)
+        except Exception as e:
+            lg.debug(e)
+            try:
+                params = parse_qs(parsedurl.query)
+                client_ip = self.client_address[0]
+                lg.debug((client_ip, params))
+                self.code = 200
 
-#############################################
+                if 'id' in params.keys():
+                    remoteid = params['id'][0]
+                    temp = params['t'][0] if 't' in params.keys() else '0'
+                    humidity = params['h'][0] if 'h' in params.keys() else '0'
+                    pressure = params['p'][0] if 'p' in params.keys() else '0'
+                    voltage = params['v'][0] if 'v' in params.keys() else '0'
+                    message = params['m'][0] if 'm' in params.keys() else ''
+                    timestamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+                    dbh = get_dbh(remoteid)
+                    c = dbh.cursor()
+                    c.execute('insert or replace into data values(?,?,?,?,?,?,?)',
+                              (timestamp, client_ip, temp, humidity, pressure, voltage, message))
+                    dbh.commit()
+                    dbh.close()
+                else:
+                    lg.debug("No id key found")
+                    self.code = 400
+            except Exception as e:
+                lg.error("GET error: %s" % e )
+                self.code = 500
+
+            self.do_HEAD()
+            self.wfile.write(b'OK.')
+
+def get_dbh(name):
+    fname = '%s/%s.sqlite3' % (args.datadir, name)
+    if os.path.isfile(fname) :
+        dbh = sqlite3.connect(fname)
+    else:
+        dbh = sqlite3.connect(fname)
+        c = dbh.cursor()
+        c.execute("""
+                    CREATE TABLE data ( timedate text primary key, ip text,
+                                        temperature real, humidity real,
+                                        pressure real, voltage real,
+                                        message text);
+                  """)
+        dbh.commit()
+    dbh.row_factory = dict_factory
+    return dbh
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx,col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 def loggerConfig(level=0, log_file=None):
     lg.setLevel({1: 'ERROR', 2: 'WARNING', 3: 'INFO'}.get(level, 'DEBUG') if level else 'CRITICAL')
