3186fd797f3ad8203356752ddbbda408e6c77d15
ConstdR <deribin@gmail.com>
2021-10-26 17:17:42 +0200

    Fix DB creation. Split loggin from listener and web

--- server/listenudp.py
+++ server/listenudp.py
@@ -15,7 +15,7 @@ import os
 
 import mqttudp.engine as me
 
-lg = logging.getLogger(__file__)
+llg = logging.getLogger(__name__)
 args = None
 cfg = None
 
@@ -31,36 +31,36 @@ def main():
     config = configparser.ConfigParser()
     config.read(args.config)
     cfg = config['listener']
-    logging.basicConfig(level=cfg['debug'])
+    llg.setLevel(cfg['debug'])
 
     me.listen(recv_packet)
 
 def recv_packet(pkt):
     global last
-    if pkt.ptype != me.PacketType.Publish:
+    if pkt.ptype != me.PacketType['Publish']:
         # not publish msg
         return
     if last.__contains__(pkt.topic) and last[pkt.topic] == pkt.value:
         # duplicate
         return
     last[pkt.topic] = pkt.value
-    lg.debug( pkt.topic+"="+pkt.value+ "\t\t" + str(pkt.addr) )
+    llg.debug( pkt.topic+"="+pkt.value+ "\t\t" + str(pkt.addr) )
 
     r = re.match(r'^weather/+([^/]*)/config$', pkt.topic)
     if r and r.group(1):
         try:
-            lg.info("Config from %s : %s" % (r.group(1), pkt.value))
+            llg.info("Config from %s : %s" % (r.group(1), pkt.value))
             store_conf(r.group(1), pkt.value)
         except Exception as e:
-            lg.error("Config store error: %s" % e)
+            llg.error("Config store error: %s" % e)
 
     r = re.match(r'^weather/+([^/]*)$', pkt.topic)
     if r and r.group(1):
-        lg.debug("Weather id: %s" % r.group(1))
+        llg.debug("Weather id: %s" % r.group(1))
         try:
             store(r.group(1), pkt.value, str(pkt.addr))
         except Exception as e:
-            lg.error("Store error: %s" % e)
+            llg.error("Store error: %s" % e)
 
 def store_conf(wid, data):
     dbname = cfg['dbdir']+'/'+wid+'.sqlite3'
@@ -75,22 +75,18 @@ def store_conf(wid, data):
 def store(wid, data, ip):
     ddata  = json.loads(data)
     ddata['ip'] = ip
-    lg.debug("JSON: %s" % ddata)
+    llg.debug("JSON: %s" % ddata)
     dbname = cfg['dbdir']+'/'+wid+'.sqlite3'
-    if not os.path.isfile(dbname):
-        dbh = sqlite3.connect(dbname)
-        c = dbh.cursor()
-        c.execute("""CREATE TABLE data ( timedate text primary key, ip text,
+    dbh = sqlite3.connect(dbname)
+    c = dbh.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS data ( timedate text primary key, ip text,
                                         temperature real, humidity real,
                                         pressure real, voltage int, voltagesun int,
                                         azimuth int, altitude int,
                                         wake int, message text)""")
-        c.execute("CREATE TABLE params (name text primary key, value text)")
-        dbh.commit()
-        dbh.close()
-        lg.debug("SQLITE created for id %s" % wid)
-    dbh = sqlite3.connect(dbname)
-    c = dbh.cursor()
+    dbh.commit()
+    c.execute("CREATE TABLE IF NOT EXISTS params (name text primary key, value text)")
+    dbh.commit()
     c.execute("""insert or replace into data (timedate, ip, temperature, humidity,
                         pressure, voltage, voltagesun, azimuth, altitude,
                         wake, message) 
